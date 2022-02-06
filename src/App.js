7373b28c64215057d66116a2d093df1ec93a0ebc
faqih15 <jahidiyam1592@gmail.com>
2022-02-06 21:03:42 +0700

    todosatu

--- src/App.js
+++ src/App.js
@@ -1,100 +1,105 @@
-import { useEffect, useRef, useState } from 'react'
-import { HiPlus } from 'react-icons/hi'
+import { useEffect, useRef, useState } from "react";
+import { HiPlus } from "react-icons/hi";
+import Card from "./Card";
+import axios from "axios";
 
-function App() {
-  const todoinputref = useRef()
-  const [todo, gantitodo] = useState([{ label: 'minum kopi', status: 'draft' }])
+export default function App() {
+  const [todo, tambahtodo] = useState([]);
+  const [warna, gantiwarna] = useState(["red", "orange", "lime"]);
 
-  const onEnter = function (e) {
-    if (e.code === "Enter") {
-      const label = e.target.value
-      const newTodo = [...todo]
-      newTodo.push({ label, status: 'draft' })
-      gantitodo(newTodo)
-      e.target.value = ''
-    }
-  }
+  useEffect(() => {
+    console.log("asw");
+    // const label = data.target.label.value;
+    // const warna = { label, warna: 0 };
+    const url = "http://localhost:3005/post";
+    axios.get(url).then((respon) => {
+      tambahtodo(respon.data);
+    });
+  }, []);
 
-  const onClick = (e) => {
-    const label = todoinputref.current.value
-    const newTodo = [...todo]
-    newTodo.push({ label, status: 'draft' })
-    gantitodo(newTodo)
-    todoinputref.current.value = ''
-  }
+  // const ganti = () => {
+  //   setindex(index + 1);
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const label = e.target.label.value
-    const newTodo = [...todo]
-    newTodo.push({ label, status: 'draft' })
-    gantitodo(newTodo)
-    e.target.label.value = ''
-  }
+  // const ganti = (i) => {
+  //   const url = "http://localhost:3005/post";
+  //   const ctodo = JSON.parse(JSON.stringify(todo));
+  //   ctodo[i].warna++;
+  //   tambahtodo(ctodo);
+  // };
 
-  const checktodo = (e, i) => {
-    const newTodo = [...todo]
-    if (e.target.checked) {
-      newTodo[i].status = 'done'
-      gantitodo(newTodo)
-    }
-  }
+  const ubah = (id, i) => {
+    const url = "http://localhost:3005/post/" + id;
+    const ctodo = JSON.parse(JSON.stringify(todo));
+    const peyek = ctodo[i];
+    console.log(peyek);
+    axios.patch(url, { warna: peyek.warna + 1 }).then((respon) => {
+      axios.get("http://localhost:3005/post").then((respon) => {
+        tambahtodo(respon.data);
+      });
+    });
+  };
 
-  const checkdraft = (e, i) => {
-    const newTodo = [...todo]
-    if (e.target.checked) {
-      newTodo[i].status = 'draft'
-      gantitodo(newTodo)
-    }
-  }
+  const fungsiTambah = (data) => {
+    const label = data.target.label.value;
+    const url = "http://localhost:3005/post";
+    const up = { label, status: "todo", warna: 0 };
+    axios
+      .post(url, up)
+      .then((respon) => {
+        axios
+          .get(url)
+          .then((respon) => {
+            tambahtodo(respon.data);
+          })
+          .catch((y) => {});
+      })
+      .catch((x) => {});
+    data.target.label.value = "";
+  };
 
-  const onDrop = () => {
-
-  }
-  
-  const onDragDraft = () => {
-
-  }
+  // const fungsitambah = (e) => {
+  //   e.preventDefault();
+  //   const label = e.target.label.value;
+  //   const ctodo = JSON.parse(JSON.stringify(todo));
+  //   ctodo.push({ label, warna: 0 });
+  //   tambahtodo(ctodo);
+  //   e.target.label.value = "";
+  // };
 
   return (
     <div className="grid grid-cols-2 gap-10">
       <div className="p-10 mx-auto w-full">
-        <div className="text-xl text-gray-500">Todo</div>
-        <form className="space-y-5 m-5 w-full" drop={onDrop} onSubmit={handleSubmit}>
-          {
-            todo.map((x, i) => {
-              return x.status === 'draft' ? <div key={i} onDrag={onDragDraft} className="flex items-center space-x-5 border border-gray-300 px-4 py-2 rounded">
-                <input type="checkbox" onChange={(e) => checktodo(e, i)} />
-                <label htmlFor="">{x.label}</label>
-              </div> : null
-            })
-          }
-          <div className="flex items-center space-x-5 border border-gray-300 px-4 py-2 rounded">
+        <div className="text-xl text-black font-extrabold">TODO</div>
+        <div className=" space-y-5 m-5 w-full">
+          {todo?.map((data, idx) => {
+            return (
+              <Card
+                key={idx}
+                bgColor={warna[data.warna % warna.length]}
+                ganti={ubah}
+                idx={idx}
+                data={data}
+              />
+            );
+          })}
+
+          <form
+            onSubmit={fungsiTambah}
+            className="outline-none bg-white flex items-center space-x-5 border border-black px-4 py-2 rounded"
+          >
             <HiPlus />
-            <input ref={todoinputref} name="label" type="text"
-              className="w-full outline-0"
-              placeholder='Masukan tugas yang akan dikerjakan'
-            // onKeyUp={onEnter}
+            <input
+              // onSubmit={clearInput}
+              name="label"
+              type="text"
+              className="outline-none w-full"
+              placeholder="Ketik tugas anda"
+              autoComplete="off"
             />
-            <button type="submit">Save</button>
-          </div>
-        </form >
-      </div>
-      <div className="p-10 mx-auto w-full">
-        <div className="text-xl text-gray-500">Done</div>
-        <div className='space-y-5 m-5 w-full'>
-          {
-            todo.map((x, i) => {
-              return x.status === 'done' ? <div key={i} className="flex items-center space-x-5 border border-gray-300 px-4 py-2 rounded">
-                <input type="checkbox" onChange={(e) => checkdraft(e, i)} />
-                <label htmlFor="">{x.label}</label>
-              </div> : null
-            })
-          }
+          </form>
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
